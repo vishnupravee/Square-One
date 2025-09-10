5d7b6abc62b9eec4dcdc866eb7e38e20a93a264c
vishnuaxobis <vishnu@axobis.com>
2025-09-10 18:06:53 +0530

    upda

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Carousel from './components/Carousel';
-import Product from './components/Product';
+
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
